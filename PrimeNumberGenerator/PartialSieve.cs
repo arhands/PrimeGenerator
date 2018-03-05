171d15969b04b76c6636e7d00a2713b43705e052
AndrewH <andrewrhands@gmail.com>
2018-03-05 11:16:42 -0600

    basic (not fast) multithreaded prime generator

---
+++ PrimeNumberGenerator/PartialSieve.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberGenerator
+{
+    class PartialSieve : PrimeProc
+    {
+        public PartialSieve(PrimeProc child)
+        {
+            Child = child;
+        }
+        static int Next { get; set; }
+        static long LastPrime { get; set; }
+        public static void Initialize()
+        {
+            Next = 0;
+            for (; Next < 10 * 1000; Next++)
+                if (!File.Exists(SaveLocation + Next + ".csv"))
+                    break;
+            if (Next == 0)
+            {
+                FirstMillionPrimes = new long[1000 * 1000];
+                FirstMillionPrimes[0] = 2;
+                int lastPrime = 2;
+                for (int i = 1; i < FirstMillionPrimes.Length; i++)
+                {
+                    lastPrime++;
+                    for (int j = 0; FirstMillionPrimes[j] * FirstMillionPrimes[j] <= lastPrime; j++)
+                    {
+                        if (lastPrime % FirstMillionPrimes[j] == 0)
+                        {
+                            lastPrime++;
+                            j = -1;
+                        }
+                    }
+                    FirstMillionPrimes[i] = lastPrime;
+                }
+                Save(FirstMillionPrimes.ToList(), 0, 0);
+                Next++;
+                LastPrime = FirstMillionPrimes.Last();
+            }
+            else
+            {
+                FirstMillionPrimes = LoadPrimes(0).ToArray();
+                LastPrime = LoadPrimes(Next - 1).Last();
+            }
+        }
+        public const string SaveLocation = "C:\\tmp\\primes\\";
+        public static long[] FirstMillionPrimes { get; set; }
+        public PrimeNumberBlock Input { get; set; }
+        PrimeProc Child { get; set; }
+        System.Threading.Thread Thread { get; set; }
+        public bool IsReadyForNextSet { get; set; }
+        public void Run()
+        {
+            Thread = new System.Threading.Thread(() =>
+            {
+                IsReadyForNextSet = true;
+                while (Input == null) { System.Threading.Thread.Sleep(1); }
+
+                while (true)
+                {
+                    PrimeNumberBlock newSet = new PrimeNumberBlock();
+                    for (int i = 0; i < Input.Primes.Count; i++)
+                        newSet.Primes.Add(Input.Primes[i]);
+                    newSet.FirstNonAbsolutePrimeIndex = Input.FirstNonAbsolutePrimeIndex;
+
+                    for (int i = newSet.NextPrimeIndex; i < 1000000; i++)
+                    {
+                        for (int j = newSet.FirstNonAbsolutePrimeIndex; j < newSet.Primes.Count; j++)
+                        {
+                            if (newSet.Primes[j] % FirstMillionPrimes[i] == 0)
+                                newSet.Primes.RemoveAt(j);
+                            else if (FirstMillionPrimes[i] * FirstMillionPrimes[i] >= newSet.Primes[j])
+                            {
+                                newSet.Primes.Add(Input.Primes[j]);
+                                newSet.FirstNonAbsolutePrimeIndex++;
+                            }
+                        }
+                        if (Child.IsReadyForNextSet)
+                        {
+                            newSet.NextPrimeIndex = i + 1;
+                            Child.Input = newSet;
+                            Input = null;
+                            Child.IsReadyForNextSet = false;
+                            IsReadyForNextSet = true;
+                            while (Input == null) { System.Threading.Thread.Sleep(1); }
+                            break;
+                        }
+                    }
+                }
+            });
+            Thread.Start();
+        }
+        const int ProcBlockSize = 1000;
+        const int SaveBlockSize = 1000 * 1000;
+        /// <summary>
+        /// this will be the first thread such that its parent = null
+        /// </summary>
+        public void Run_First()
+        {
+            Thread = new System.Threading.Thread(() =>
+            {
+                long NextNumber = LastPrime + 1;
+                while (NextNumber >= 0)
+                {
+                    PrimeNumberBlock newSet = new PrimeNumberBlock();
+                    newSet.Primes = new List<long>(ProcBlockSize);
+                    for (int i = 1; i <= ProcBlockSize; i++)
+                        newSet.Primes.Add(i + NextNumber);
+                    NextNumber += ProcBlockSize;
+                    newSet.FirstNonAbsolutePrimeIndex = 0;
+                    bool tooFast = true;
+                    for (int i = 0; i < FirstMillionPrimes.Length; i++)
+                    {
+                        for (int j = newSet.FirstNonAbsolutePrimeIndex; j < newSet.Primes.Count; j++)
+                        {
+                            if (newSet.Primes[j] % FirstMillionPrimes[i] == 0)
+                            {
+                                newSet.Primes.RemoveAt(j);
+                                j--;
+                            }
+                            else if (FirstMillionPrimes[i] * FirstMillionPrimes[i] >= newSet.Primes[j])
+                            {
+                                newSet.FirstNonAbsolutePrimeIndex++;
+                            }
+                        }
+                        if (Child.IsReadyForNextSet)
+                        {
+                            newSet.NextPrimeIndex = i + 1;
+                            Child.Input = newSet;
+                            while (Child.IsReadyForNextSet)
+                            {
+                                System.Threading.Thread.Sleep(1);
+                            }
+                            tooFast = false;
+                            break;
+                        }
+
+                    }
+                    if (tooFast)
+                    {
+                        while (!Child.IsReadyForNextSet) { System.Threading.Thread.Sleep(1); }
+                        newSet.NextPrimeIndex = newSet.Primes.Count;
+                        Child.Input = newSet;
+                        while (Child.IsReadyForNextSet) { System.Threading.Thread.Sleep(1); }
+                    }
+                }
+            });
+            Thread.Start();
+        }
+        /// <summary>
+        /// this will be the last thread such that Child = null
+        /// </summary>
+        public void Run_Last()
+        {
+            Thread = new System.Threading.Thread(() =>
+            {
+                PrimeNumberBlock newSet = new PrimeNumberBlock();
+                while (true)
+                {
+                    IsReadyForNextSet = true;
+                    while (Input == null) { System.Threading.Thread.Sleep(1); }
+                    int oldLength = newSet.Primes.Count;
+                    for (int i = 0; i < Input.Primes.Count; i++)
+                        newSet.Primes.Add(Input.Primes[i]);
+                    newSet.FirstNonAbsolutePrimeIndex = oldLength + Input.FirstNonAbsolutePrimeIndex;
+                    newSet.NextPrimeIndex = Input.NextPrimeIndex;
+                    Input = null;
+                    IsReadyForNextSet = false;
+
+                    for (int i = newSet.FirstNonAbsolutePrimeIndex; i < newSet.Primes.Count; i++)
+                    {
+                        for (int j = newSet.NextPrimeIndex; j < FirstMillionPrimes.Length; j++)
+                        {
+                            if (FirstMillionPrimes[j] * FirstMillionPrimes[j] > newSet.Primes[i])
+                                break;
+                            if (newSet.Primes[i] % FirstMillionPrimes[j] == 0)
+                            {
+                                newSet.Primes.RemoveAt(i);
+                                i--;
+                                break;
+                            }
+                        }
+
+                    }
+                    if (newSet.Primes.Count >= SaveBlockSize)
+                    {
+                        Save(newSet.Primes.GetRange(0, SaveBlockSize), Next * SaveBlockSize, Next);
+                        Next++;
+                        newSet.Primes.RemoveRange(0, SaveBlockSize);
+                    }
+                }
+            });
+            Thread.Start();
+        }
+        static void Save(List<long> primes, long firstPrimeIndex, int name)
+        {
+            string[] text = new string[primes.Count + 1];
+            text[0] = "\"Index\", \" Prime \"";
+            for (int i = 0; i < primes.Count; i++)
+            {
+                text[i + 1] = "\"" + (firstPrimeIndex + i) + "\", \"" + primes[i] + "\"";
+            }
+            File.WriteAllLines(SaveLocation + name + ".csv", text);
+        }
+        public static List<long> LoadPrimes(int title)
+        {
+
+            List<long> primes = new List<long>();
+            string[] text = File.ReadAllLines(SaveLocation + title + ".csv", Encoding.ASCII);
+            for (int i = 1; i < text.Length; i++)
+            {
+                string number = text[i].Substring(text[i].IndexOf(",") + 3);
+                number = number.Substring(0, number.IndexOf("\""));
+                primes.Add(Convert.ToInt64(number));
+            }
+            return primes;
+        }
+    }
+    class PrimeNumberBlock
+    {
+        public PrimeNumberBlock()
+        {
+            Primes = new List<long>();
+        }
+        public List<long> Primes { get; set; }
+        /// <summary>
+        /// index for the first number in the next thread to test
+        /// </summary>
+        public int NextPrimeIndex { get; set; }
+        public int FirstNonAbsolutePrimeIndex { get; set; }
+    }
+    interface PrimeProc
+    {
+        /// <summary>
+        /// when the child is ready for the next prime block, it will set this to true
+        /// </summary>
+        bool IsReadyForNextSet { get; set; }
+        PrimeNumberBlock Input { get; set; }
+    }
+}
---
+++ PrimeNumberGenerator/PrimeGenerator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace PrimeNumberGenerator
+{
+    class PrimeGenerator
+    {
+        /// <summary>
+        /// this is the span from the starting number to the ending number for example,
+        /// if the span was 10 then the first set would be {2,3,5,7}
+        /// </summary>
+        public const int SetNumberSpan = 1000000;
+        public const string SaveLocation = "C:\\tmp\\primes\\";
+        public static List<long> _firstPrimeSet;
+        public bool Finished { get; private set; }
+        public PrimeSet Primes { get; private set; }
+        System.Threading.Thread _thread;
+        public void CollectBlock(int block)
+        {
+            _thread = new System.Threading.Thread(() =>
+            {
+                Finished = false;
+                //setting span for generation
+                long start = SetNumberSpan * block, end = start + SetNumberSpan;
+                //generating primes
+                var primes = new PrimeSet();
+                primes.AverageGenerationTime = (((DateTime.Now.Day * 24 + DateTime.Now.Hour) * 60 + DateTime.Now.Minute) * 60 + DateTime.Now.Second) * 1000 + DateTime.Now.Millisecond;
+                primes.Primes = new List<long>();  
+                for (long j = start; j <= end; j++)
+                {
+                    bool prime = true;
+                    for (int h = 0; _firstPrimeSet[h] * _firstPrimeSet[h] <= j; h++)
+                    {
+                        if (j % _firstPrimeSet[h] == 0)
+                        {
+                            prime = false;
+                            break;
+                        }
+                    }
+                    if (prime)
+                        primes.Primes.Add(j);
+                }
+                primes.AverageGenerationTime = (((DateTime.Now.Day * 24 + DateTime.Now.Hour) * 60 + DateTime.Now.Minute) * 60 + DateTime.Now.Second) * 1000 + DateTime.Now.Millisecond - primes.AverageGenerationTime;
+                primes.TotalGenerationTime = primes.AverageGenerationTime;
+                primes.AverageGenerationTime = primes.AverageGenerationTime / primes.Primes.Count;
+                Finished = true;
+            });
+            _thread.Start();
+        }
+        public static List<long> LoadPrimes(int title)
+        {
+
+            List<long> primes = new List<long>();
+            string[] text = File.ReadAllLines(SaveLocation + title + ".csv", Encoding.ASCII);
+            for (int i = 1; i < text.Length; i++)
+            {
+                string number = text[i].Substring(1);
+                number = number.Substring(0, number.IndexOf("\""));
+                primes.Add(Convert.ToInt64(number));
+            }
+            return primes;
+        }
+        public static PrimeSet GenerateInitialPrimeSet()
+        {
+            PrimeSet primes = new PrimeSet();
+            int dt = (((DateTime.Now.Day * 24 + DateTime.Now.Hour) * 60 + DateTime.Now.Minute) * 60 + DateTime.Now.Second) * 1000 + DateTime.Now.Millisecond;
+            primes.Primes = _firstPrimeSet = new List<long>();
+            primes.Primes.Add(2);
+            for (long j = 3; j <= SetNumberSpan; j++)
+            {
+                bool prime = true;
+                for (int h = 0; _firstPrimeSet[h] * _firstPrimeSet[h] <= j; h++)
+                {
+                    if (j % _firstPrimeSet[h] == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                    primes.Primes.Add(j);
+            }
+            dt = (((DateTime.Now.Day * 24 + DateTime.Now.Hour) * 60 + DateTime.Now.Minute) * 60 + DateTime.Now.Second) * 1000 + DateTime.Now.Millisecond - dt;
+            primes.TotalGenerationTime = dt;
+            primes.AverageGenerationTime = dt / primes.Primes.Count;
+            return primes;
+
+        }
+    }
+    public class PrimeSet
+    {
+        public List<long> Primes { get; set; }
+        /// <summary>
+        /// this is in milliseconds
+        /// </summary>
+        public double TotalGenerationTime { get; set; }
+        /// <summary>
+        /// this is in milliseconds
+        /// </summary>
+        public double AverageGenerationTime { get; set; }
+    }
+}
